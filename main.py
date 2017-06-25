8cc210e354236b687858042f93d8eb07af2d9aad
Marcus Danilo <mdanilor@gmail.com>
2017-06-25 01:13:57 -0300

    Debugging...

--- main.py
+++ main.py
@@ -31,14 +31,17 @@ def run():
         link = 0 #Just a random initial value.
         while linkCount < 1:
             link = Persistency.getLink(hs)
+            print "%s: Thread %s just got a new link" % (datetime.datetime.now(), threadNum)
             if link is None:
                 break
             linkCount += 1 #MAX OF 100 LINKS PER DOMAIN
             processor.setLink(link[1])
             content = TorUrlProcessor.getContent(link[1])
+            print "%s: Thread %s just got content" % (datetime.datetime.now(), threadNum)
 
             if content == 0: #In case there was a problem getting the link content
                 Persistency.saveLink(link, None, None, 3)
+                print "%s: Thread %s link was broken" % (datetime.datetime.now(), threadNum)
                 break
             if "content-type: text" not in str(content[0]).lower(): #In case we got content, but it's not readable text
                 Persistency.saveLink(link, None, content, 4)
@@ -48,10 +51,13 @@ def run():
             except Exception:
                 continue
 
+                print "%s: Thread %s just processed content" % (datetime.datetime.now(), threadNum)
+
             title = processor.title #Getting data
             for newLink in processor.links:
                 Persistency.newLink(newLink) #Potential new links to process
             Persistency.saveLink(link, title, content, 2) #Saving this link's data.
+            print "%s: Thread %s just saved everything" % (datetime.datetime.now(), threadNum)
 
 threadCount = ConfigLoader.threadcount
 
